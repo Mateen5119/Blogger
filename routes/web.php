c1a2f099c9e353f9131d8ba0e7af99ac0505cdfd
Mateen5119 <111520268+Mateen5119@users.noreply.github.com>
2026-04-10 16:19:11 +0500

    Model-View-Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
-
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get("/", [BlogController::class, 'index']);
